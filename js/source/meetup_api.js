0adc7f81a5757fa8762dc771b0c913d9a97fb4e5
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2016-04-09 14:13:42 +0200

    feat(app): New file for Meetup API

---
+++ js/source/meetup_api.js
@@ -0,0 +1,62 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;( function( $, window, document, undefined ) {
+
+	"use strict";
+
+		// undefined is used here as the undefined global variable in ECMAScript 3 is
+		// mutable (ie. it can be changed by someone else). undefined isn't really being
+		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+		// can no longer be modified.
+
+		// window and document are passed through as local variables rather than global
+		// as this (slightly) quickens the resolution process and can be more efficiently
+		// minified (especially when both are regularly referenced in your plugin).
+
+		// Create the defaults once
+		var pluginName = "meetupApi",
+			defaults = {
+				propertyName: "value"
+			};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+			this.element = element;
+
+			// jQuery has an extend method which merges the contents of two or
+			// more objects, storing the result in the first object. The first object
+			// is generally empty as we don't want to alter the default options for
+			// future instances of the plugin
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend( Plugin.prototype, {
+			init: function() {
+				requestEvents();
+			},
+
+			requestEvents: function() {
+				$.getJSON('https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=webmeetup&page=5', function(data) {
+					console.log(data);
+				});
+			}
+		} );
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			} );
+		};
+
+		registerplugin(defaults.target, pluginName);
+
+} )( jQuery, window, document );
